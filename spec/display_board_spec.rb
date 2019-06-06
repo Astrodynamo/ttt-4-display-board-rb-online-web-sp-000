6a7d7bbaa6f120d3ee2eb204f8c1d1ac3a97340b
Amanda King <amapickell@gmail.com>
2019-06-06 01:46:00 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -173,10 +173,11 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[4]).to eq(" X | X | X ")
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      #expect(true).to be(true)
     end
 
     it 'prints an entire board full of Os' do
+      
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
 
@@ -185,7 +186,7 @@ describe "#display_board in 'lib/display_board.rb" do
       # example's situation.
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      #expect(true).to be(true)
     end
   end
 end
