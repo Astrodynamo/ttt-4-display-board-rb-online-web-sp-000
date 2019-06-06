025daee01857a8a100158cd120e6c8bb5d64fba4
Amanda King <amapickell@gmail.com>
2019-06-06 01:35:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,3 @@
 def display_board (board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  puts "#{board[0]}|#{board[1]}|#{board[2]}"
 end
